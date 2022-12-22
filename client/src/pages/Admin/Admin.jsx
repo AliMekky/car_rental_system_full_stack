18604a876b444addedcc61e091cda458917db9b5
Lara Hossam Hassan <83987195+LaraHossam@users.noreply.github.com>
2022-12-22 20:28:48 +0200

    advanced search

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useState } from "react";
 import "./Admin.css";
+import Checkbox from "@mui/material/Checkbox";
+
 import Navbar from "../../components/navbar/Navbar";
 import CustomerPopup from "./CustomerPopup";
 import CarPopup from "./CarPopup";
@@ -14,107 +15,251 @@ import Chart from "./Assets/chart-up-svgrepo-com.svg";
 import OneDatePopup from "./OneDatePopup";
 import UpdateStatus from "../UpdateStatus/UpdateStatus";
 import AddCar from "../AddCar/AddCar";
-
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 
 function Admin() {
-    /////for status on a specific date///
-    const [StatusOpen, setStatus] = useState(false);
-    ////// for any car on a specific period///
-    const [CarOpen, setCar] = useState(false);
-    ///// for all resrvations on a specific period////
-    const [DateOpen, setDate] = useState(false);
-    //////for all customer reservation////
-    const [CustOpen, setCust] = useState(false);
-    ///// for daily payments//////
-    const [PaymentOpen, setPayment] = useState(false);
-    /// for add new car//
-    const [NewCarOPen, setNewCar] = useState(false);
-    //// for update status///
-    const [UpdatStatusOPen, setUpdateStatus] = useState(false);
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-    const handleClose = () => {
-        setStatus(false);
-        setCar(false);
-        setDate(false);
-        setCust(false);
-        setPayment(false);
-        setNewCar(false);
-        setUpdateStatus(false);
-    };
+  /////for status on a specific date///
+  const [StatusOpen, setStatus] = useState(false);
+  ////// for any car on a specific period///
+  const [CarOpen, setCar] = useState(false);
+  ///// for all resrvations on a specific period////
+  const [DateOpen, setDate] = useState(false);
+  //////for all customer reservation////
+  const [CustOpen, setCust] = useState(false);
+  ///// for daily payments//////
+  const [PaymentOpen, setPayment] = useState(false);
+  /// for add new car//
+  const [NewCarOPen, setNewCar] = useState(false);
+  //// for update status///
+  const [UpdatStatusOPen, setUpdateStatus] = useState(false);
 
-    return (
-        <div>
-            <Navbar />
+  const handleClose = () => {
+    setStatus(false);
+    setCar(false);
+    setDate(false);
+    setCust(false);
+    setPayment(false);
+    setNewCar(false);
+    setUpdateStatus(false);
+  };
 
-            <div className=" container  justify-content-center align-items-center ">
-                <div className="row justify-content-center">
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
-                        <div className="card shadow-2-strong report">
-                            <div className=" card-body p-5 text-center card-size " onClick={() => { setNewCar(true); }}>
-                                <img src={Car} alt="car" className="image-spacing" style={{"marginBottom":"5%"}}></img>
-                                <h2> Add new car </h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
-                        <div className="card shadow-2-strong report">
-                            <div className=" card-body p-5 text-center card-size" onClick={() => { setUpdateStatus(true); }}>
-                                <img src={Update} alt="update" className="image-spacing" style={{"marginBottom":"5%"}} ></img>
-                                <h2> update status </h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
-                        <div className="card shadow-2-strong report" onClick={() => { setStatus(true); }}>
-                            <div className=" card-body p-5 text-center card-size ">
-                                <img src={Status} alt="status" className="image-spacing" style={{"marginBottom":"5%"}} ></img>
-                                <h2>  car status </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
-                        <div className="card shadow-2-strong report" onClick={() => { setDate(true); }}>
-                            <div className=" card-body p-5 text-center card-size">
-                                <img src={Chart} alt="chart" className="image-spacing" ></img>
-                                <h2> Browse by date </h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4 h-100">
-                        <div className="card shadow-2-strong report" onClick={() => { setCar(true); }}>
-                            <div className=" card-body p-5 text-center card-size">
-                                <img src={Chart} alt="chart"   className="image-spacing" style={{"marginBottom":"5%"}}></img>
-                                <h2> Browse by car </h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" col-8  col-md-8 col-sm-8 col-lg-4 col-xl-4 h-100">
-                        <div className="card shadow-2-strong report" onClick={() => {
-                            setCust(true);
-                        }}>
-                            <div className=" card-body p-5 text-center card-size ">
-                                <img src={Customer} alt="customer" className="image-spacing"></img>
-                                <h2>  Browse by customer  </h2>
-                            </div>
-                        </div>
+  const [key, setKey] = useState("");
+  const navigate = useNavigate();
+  const [selectedValues, setSelectedValues] = useState([]);
+  const options = [
+    { value: "car", label: "Car" },
+    { value: "customer", label: "Customer" },
+    { value: "day", label: "Reservation Day" },
+  ];
+  // Handle changes to the checkbox list
+  const handleChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedValues([...selectedValues, value]);
+    } else {
+      setSelectedValues(selectedValues.filter((v) => v !== value));
+    }
+  };
 
-                    </div>
-                    <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4 ">
-                        <div className="card shadow-2-strong report" onClick={() => { setPayment(true); }}>
-                            <div className=" card-body p-5 text-center card-size">
-                                <img src={Money} alt="Money" className="image-spacing"  ></img>
-                                <h2> Daily payments </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const advanced = (event) => {
+    if (key != "") {
+      axios
+        .get("http://localhost:4000/search", {
+          params: {
+            key: key,
+            selectedValues: selectedValues
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.length == 0) {
+            return;
+          }
+          navigate("/Table", {
+            state: {
+              info: response.data,
+              title: "All record containing " + key,
+            },
+          });
+        });
+    }
+  };
 
-                {/* <div className="row justify-content-start">
+  return (
+    <div>
+      <Navbar />
+      <br />
+      <div class="container">
+        <div class="align-items-center">
+            
+        <form class="elem">
+                {options.map((option) => (
+                  <li key={option.value}>
+                    <input
+                      type="checkbox"
+                      value={option.value}
+                      checked={selectedValues.includes(option.value)}
+                      onChange={handleChange}
+                    />
+                    {option.label}
+                    
+                  </li>
+                ))}
+              </form>
+        </div>
+        <div class="row height d-flex justify-content-center align-items-center">
+          <div class="col-md-8">
+            <div class="search">
+              <i class="fa fa-search"></i>
+              <input
+                onChange={(e) => {
+                  setKey(e.target.value);
+                }}
+                type="text"
+                class="form-control"
+                placeholder="Search by car details, customer details or reservation day"
+              />
+              <div></div>
+              
+              <button onClick={advanced} class="btn btn-primary">
+                Go
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" container  justify-content-center align-items-center ">
+        <div className="row justify-content-center">
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
+            <div className="card shadow-2-strong report">
+              <div
+                className=" card-body p-5 text-center card-size "
+                onClick={() => {
+                  setNewCar(true);
+                }}
+              >
+                <img
+                  src={Car}
+                  alt="car"
+                  className="image-spacing"
+                  style={{ marginBottom: "5%" }}
+                ></img>
+                <h2> Add new car </h2>
+              </div>
+            </div>
+          </div>
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
+            <div className="card shadow-2-strong report">
+              <div
+                className=" card-body p-5 text-center card-size"
+                onClick={() => {
+                  setUpdateStatus(true);
+                }}
+              >
+                <img
+                  src={Update}
+                  alt="update"
+                  className="image-spacing"
+                  style={{ marginBottom: "5%" }}
+                ></img>
+                <h2> update status </h2>
+              </div>
+            </div>
+          </div>
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
+            <div
+              className="card shadow-2-strong report"
+              onClick={() => {
+                setStatus(true);
+              }}
+            >
+              <div className=" card-body p-5 text-center card-size ">
+                <img
+                  src={Status}
+                  alt="status"
+                  className="image-spacing"
+                  style={{ marginBottom: "5%" }}
+                ></img>
+                <h2> car status </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4">
+            <div
+              className="card shadow-2-strong report"
+              onClick={() => {
+                setDate(true);
+              }}
+            >
+              <div className=" card-body p-5 text-center card-size">
+                <img src={Chart} alt="chart" className="image-spacing"></img>
+                <h2> Browse by date </h2>
+              </div>
+            </div>
+          </div>
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4 h-100">
+            <div
+              className="card shadow-2-strong report"
+              onClick={() => {
+                setCar(true);
+              }}
+            >
+              <div className=" card-body p-5 text-center card-size">
+                <img
+                  src={Chart}
+                  alt="chart"
+                  className="image-spacing"
+                  style={{ marginBottom: "5%" }}
+                ></img>
+                <h2> Browse by car </h2>
+              </div>
+            </div>
+          </div>
+          <div className=" col-8  col-md-8 col-sm-8 col-lg-4 col-xl-4 h-100">
+            <div
+              className="card shadow-2-strong report"
+              onClick={() => {
+                setCust(true);
+              }}
+            >
+              <div className=" card-body p-5 text-center card-size ">
+                <img
+                  src={Customer}
+                  alt="customer"
+                  className="image-spacing"
+                ></img>
+                <h2> Browse by customer </h2>
+              </div>
+            </div>
+          </div>
+          <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4 ">
+            <div
+              className="card shadow-2-strong report"
+              onClick={() => {
+                setPayment(true);
+              }}
+            >
+              <div className=" card-body p-5 text-center card-size">
+                <img src={Money} alt="Money" className="image-spacing"></img>
+                <h2> Daily payments </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="row justify-content-start">
 
                     <div className=" col-8 col-md-8 col-sm-8 col-lg-4 col-xl-4 ">
                         <div className="card shadow-2-strong report" onClick={() => { setPayment(true); }}>
@@ -125,65 +270,32 @@ function Admin() {
                         </div>
                     </div>
                 </div> */}
-
-            </div>
-            {CustOpen && (
-                <CustomerPopup
-
-                    close={handleClose}
-                />
-            )}
-            {CarOpen && (
-                <CarPopup
-
-                    close={handleClose}
-                />
-            )}
-            {DateOpen && (
-                <DatTimePopup
-
-                   close={handleClose}
-                    title={"Generate a report on all reservations within a specific period"}
-                />
-            )}
-            {PaymentOpen && (
-                <DatTimePopup
-
-                    close={handleClose}
-                    title={"Generate a report on daily payments within a specific period"}
-                />
-            )}
-            {StatusOpen && (
-                <OneDatePopup
-
-                    close={handleClose}
-                    title={"Generate a report on daily payments within a specific period"}
-                />
-            )}
-            {UpdatStatusOPen && (
-                <UpdateStatus
-
-                    close={handleClose}
-                    
-                />
-            )}
-            {NewCarOPen && (
-                <AddCar
-
-                    close={handleClose}
-                    
-                />
-            )}
-            
-        </div>
-
-
-
-
-
-
-
-
-    )
+      </div>
+      {CustOpen && <CustomerPopup close={handleClose} />}
+      {CarOpen && <CarPopup close={handleClose} />}
+      {DateOpen && (
+        <DatTimePopup
+          close={handleClose}
+          title={
+            "Generate a report on all reservations within a specific period"
+          }
+        />
+      )}
+      {PaymentOpen && (
+        <DatTimePopup
+          close={handleClose}
+          title={"Generate a report on daily payments within a specific period"}
+        />
+      )}
+      {StatusOpen && (
+        <OneDatePopup
+          close={handleClose}
+          title={"Generate a report on daily payments within a specific period"}
+        />
+      )}
+      {UpdatStatusOPen && <UpdateStatus close={handleClose} />}
+      {NewCarOPen && <AddCar close={handleClose} />}
+    </div>
+  );
 }
 export default Admin;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 
@@ -8,11 +8,11 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "gocar"
+  database: "gocar",
 });
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -24,29 +24,33 @@ db.connect((err) => {
   console.log("Connection done");
 });
 
-
-app.get('/browseCar', (req, res) => {
+app.get("/browseCar", (req, res) => {
   res.json({
-    "name": "BMW my car",
-    "msg": "This is my first tweet!",
-    "username": "codrkai"
-})
+    name: "BMW my car",
+    msg: "This is my first tweet!",
+    username: "codrkai",
+  });
 });
 
 app.get("/customerReservations", (req, res) => {
   let name = req.query.name;
   let email = req.query.email;
-  let stat = "SELECT PICKUP_LOCATION,PICKUP_DATE,DROPOFF_LOCATION,DROPOFF_DATE,RESERVATION_DAY,PLATE_ID,MANUFACTURER,MODEL FROM RESERVATION NATURAL JOIN CUSTOMER NATURAL JOIN CAR WHERE NAME=\"" + name + "\" AND EMAIL=\""+email+"\"";
+  let stat =
+    'SELECT PICKUP_LOCATION,PICKUP_DATE,DROPOFF_LOCATION,DROPOFF_DATE,RESERVATION_DAY,PLATE_ID,MANUFACTURER,MODEL FROM RESERVATION NATURAL JOIN CUSTOMER NATURAL JOIN CAR WHERE NAME="' +
+    name +
+    '" AND EMAIL="' +
+    email +
+    '"';
   console.log(stat);
   db.query(stat, (err, rows) => {
     if (!err) {
-        var result = JSON.parse(JSON.stringify(rows));
+      var result = JSON.parse(JSON.stringify(rows));
     } else {
-        console.log(err)
+      console.log(err);
     }
-    console.log('Reservations are: \n', result);
+    console.log("Reservations are: \n", result);
     res.send(result);
-})
+  });
 });
 
 app.get("/carReservations", (req, res) => {
@@ -57,22 +61,35 @@ app.get("/carReservations", (req, res) => {
   let endDate = req.query.endDate;
   let startTime = req.query.startTime;
   let endTime = req.query.endTime;
-  let start = startDate + " "+startTime;
-  let end = endDate + " "+endTime;
+  let start = startDate + " " + startTime;
+  let end = endDate + " " + endTime;
 
   // let stat = "SELECT PICKUP_LOCATION,PICKUP_DATE,DROPOFF_LOCATION,DROPOFF_DATE,RESERVATION_DAY FROM RESERVATION NATURAL JOIN CAR WHERE PLATE_ID=\"" + plate_id + "\"" + "AND MANUFACTURER=\"" + man + "\"" + "AND MODEL=\"" +model + "\"";
-  let stat = " SELECT RESERVATION_DAY, PICKUP_DATE, PICKUP_LOCATION, DROPOFF_DATE, DROPOFF_LOCATION PLATE_ID, MANUFACTURER, MODEL, YEAR, TYPE, CAPACITY FROM RESERVATION NATURAL JOIN CAR WHERE PICKUP_DATE>'"+ start +"' AND DROPOFF_DATE<'"+end+"' AND PLATE_ID=\"" + plate_id + "\"" + "AND MANUFACTURER=\"" + man + "\"" + "AND MODEL=\"" +model + "\"";
+  let stat =
+    " SELECT RESERVATION_DAY, PICKUP_DATE, PICKUP_LOCATION, DROPOFF_DATE, DROPOFF_LOCATION PLATE_ID, MANUFACTURER, MODEL, YEAR, TYPE, CAPACITY FROM RESERVATION NATURAL JOIN CAR WHERE PICKUP_DATE>'" +
+    start +
+    "' AND DROPOFF_DATE<'" +
+    end +
+    "' AND PLATE_ID=\"" +
+    plate_id +
+    '"' +
+    'AND MANUFACTURER="' +
+    man +
+    '"' +
+    'AND MODEL="' +
+    model +
+    '"';
 
   console.log(stat);
   db.query(stat, (err, rows) => {
     if (!err) {
-        var result = JSON.parse(JSON.stringify(rows));
+      var result = JSON.parse(JSON.stringify(rows));
     } else {
-        console.log(err)
+      console.log(err);
     }
-    console.log('Reservations are: \n', result);
+    console.log("Reservations are: \n", result);
     res.send(result);
-})
+  });
 });
 
 app.get("/reservations", (req, res) => {
@@ -80,30 +97,194 @@ app.get("/reservations", (req, res) => {
   let endDate = req.query.endDate;
   let startTime = req.query.startTime;
   let endTime = req.query.endTime;
-  let start = startDate + " "+startTime;
-  let end = endDate + " "+endTime;
+  let start = startDate + " " + startTime;
+  let end = endDate + " " + endTime;
   // let stat = " SELECT STR_TO_DATE(\""+start+"\", '%Y-%m-%d %T') AS startT\; SELECT STR_TO_DATE(\""+end+"\", '%Y-%m-%d %T') AS endT\; SELECT * FROM RESERVATION WHERE PICKUP_DATE< startT AND DROPOFF_DATE>endT\;"
 
-
-  let stat = " SELECT RESERVATION_DAY, PICKUP_DATE, PICKUP_LOCATION, DROPOFF_DATE, DROPOFF_LOCATION PLATE_ID, MANUFACTURER, MODEL, YEAR, TYPE, CAPACITY, NAME, EMAIL, PHONE_NUMBER FROM RESERVATION NATURAL JOIN CAR NATURAL JOIN CUSTOMER WHERE PICKUP_DATE>'"+ start +"' AND DROPOFF_DATE<'"+end+"'";
+  let stat =
+    " SELECT RESERVATION_DAY, PICKUP_DATE, PICKUP_LOCATION, DROPOFF_DATE, DROPOFF_LOCATION PLATE_ID, MANUFACTURER, MODEL, YEAR, TYPE, CAPACITY, NAME, EMAIL, PHONE_NUMBER FROM RESERVATION NATURAL JOIN CAR NATURAL JOIN CUSTOMER WHERE PICKUP_DATE>'" +
+    start +
+    "' AND DROPOFF_DATE<'" +
+    end +
+    "'";
   console.log(stat);
   db.query(stat, (err, rows) => {
     if (!err) {
-        var result = JSON.parse(JSON.stringify(rows));
+      var result = JSON.parse(JSON.stringify(rows));
     } else {
-        console.log(err)
+      console.log(err);
     }
-    console.log('Reservations are: \n', result);
+    console.log("Reservations are: \n", result);
     res.send(result);
-})
+  });
 });
 
-app.post("/login",(req,res)=>{
+app.get("/search", (req, res) => {
+  let key = req.query.key;
+  let selectedValues = req.query.selectedValues;
+  console.log(selectedValues);
+  let stat1 =
+    '(SELECT NAME, EMAIL, PHONE_NUMBER FROM customer WHERE NAME LIKE "%' +
+    key +
+    '%" OR EMAIL LIKE "%' +
+    key +
+    '%" OR PHONE_NUMBER LIKE "%' +
+    key +
+    '%")';
+  let stat2 =
+    '(SELECT PLATE_ID, MANUFACTURER, MODEL, YEAR, PRICE, TYPE, CAPACITY, COLOR FROM CAR WHERE PLATE_ID LIKE "%' +
+    key +
+    '%" OR MANUFACTURER LIKE "%' +
+    key +
+    '%" OR MODEL LIKE "%' +
+    key +
+    '%"' +
+    'OR YEAR LIKE "%' +
+    key +
+    '%" OR PRICE LIKE "%' +
+    key +
+    '%"' +
+    'OR CAPACITY LIKE "%' +
+    key +
+    '%" OR TYPE LIKE "%' +
+    key +
+    '%");';
+  let stat3 =
+    '(SELECT RESERVATION_DAY, PICKUP_LOCATION,PICKUP_DATE, DROPOFF_LOCATION, DROPOFF_DATE, PLATE_ID, NAME, PHONE_NUMBER, EMAIL FROM RESERVATION NATURAL JOIN CUSTOMER WHERE RESERVATION_DAY LIKE "%' +
+    key +
+    '%");';
+  let stat4 =
+    '(SELECT NAME, EMAIL, PHONE_NUMBER, PLATE_ID, MANUFACTURER, MODEL, YEAR, PRICE, TYPE, CAPACITY, COLOR  FROM customer natural join car WHERE NAME LIKE "%' +
+    key +
+    '%" OR EMAIL LIKE "%' +
+    key +
+    '%" OR PHONE_NUMBER LIKE "%' +
+    key +
+    '%" OR PLATE_ID LIKE "%' +
+    key +
+    '%" OR MANUFACTURER LIKE "%' +
+    key +
+    '%" OR MODEL LIKE "%' +
+    key +
+    '%"' +
+    'OR YEAR LIKE "%' +
+    key +
+    '%" OR PRICE LIKE "%' +
+    key +
+    '%"' +
+    'OR CAPACITY LIKE "%' +
+    key +
+    '%" OR TYPE LIKE "%' +
+    key +
+    '%");';
+  let stat5 =
+    '(SELECT PLATE_ID, MANUFACTURER, MODEL, YEAR, PRICE, TYPE, CAPACITY, COLOR, RESERVATION_DAY, PICKUP_LOCATION,PICKUP_DATE, DROPOFF_LOCATION, DROPOFF_DATE, PLATE_ID FROM CAR NATURAL  JOIN RESERVATION WHERE PLATE_ID LIKE "%' +
+    key +
+    '%" OR MANUFACTURER LIKE "%' +
+    key +
+    '%" OR MODEL LIKE "%' +
+    key +
+    '%"' +
+    'OR YEAR LIKE "%' +
+    key +
+    '%" OR PRICE LIKE "%' +
+    key +
+    '%"' +
+    'OR CAPACITY LIKE "%' +
+    key +
+    '%" OR TYPE LIKE "%' +
+    key +
+    '%" OR RESERVATION_DAY LIKE "%' +
+    key +
+    '%");';
+  let stat6 =
+    '(SELECT NAME, EMAIL, PHONE_NUMBER, RESERVATION_DAY, PICKUP_LOCATION,PICKUP_DATE, DROPOFF_LOCATION, DROPOFF_DATE, PLATE_ID FROM CUSTOMER NATURAL  JOIN RESERVATION WHERE PLATE_ID LIKE "%' +
+    key +
+    '%" OR NAME LIKE "%' +
+    key +
+    '%" OR EMAIL LIKE "%' +
+    key +
+    '%"' +
+    'OR PHONE_NUMBER LIKE "%' +
+    key +
+    '%" OR RESERVATION_DAY LIKE "%' +
+    key +
+    '%");';
+  let stat7 =
+    '(SELECT RESERVATION_DAY, PICKUP_LOCATION,PICKUP_DATE, DROPOFF_LOCATION, DROPOFF_DATE, NAME, EMAIL, PHONE_NUMBER, PLATE_ID, MANUFACTURER, MODEL, YEAR, PRICE, TYPE, CAPACITY, COLOR  FROM customer natural  join car natural  join reservation WHERE NAME LIKE "%' +
+    key +
+    '%" OR EMAIL LIKE "%' +
+    key +
+    '%" OR PHONE_NUMBER LIKE "%' +
+    key +
+    '%" OR PLATE_ID LIKE "%' +
+    key +
+    '%" OR MANUFACTURER LIKE "%' +
+    key +
+    '%" OR MODEL LIKE "%' +
+    key +
+    '%"' +
+    'OR YEAR LIKE "%' +
+    key +
+    '%" OR PRICE LIKE "%' +
+    key +
+    '%"' +
+    'OR CAPACITY LIKE "%' +
+    key +
+    '%" OR TYPE LIKE "%' +
+    key +
+    '%" OR RESERVATION_DAY LIKE "%' +
+    key +
+    '%");';
+  let final = [];
+  if (selectedValues.length == 1) {
+    if (selectedValues[0] == "car") {
+      final = stat2;
+    }
+    if (selectedValues[0] == "customer") {
+      final = stat1;
+    }
+    if (selectedValues[0] == "day") {
+      final = stat3;
+    }
+  }
+  if (selectedValues.length == 2) {
+    if (selectedValues[1] == "car" && selectedValues[0] == "customer") {
+      final = stat4;
+    }
+    if (selectedValues[0] == "car" && selectedValues[1] == "customer") {
+      final = stat4;
+    }
+    if (selectedValues[1] == "car" && selectedValues[0] == "day") {
+      final = stat5;
+    }
+    if (selectedValues[0] == "car" && selectedValues[1] == "day") {
+      final = stat5;
+    }
+    if (selectedValues[1] == "customer" && selectedValues[0] == "day") {
+      final = stat6;
+    }
+    if (selectedValues[0] == "customer" && selectedValues[1] == "day") {
+      final = stat6;
+    }
+  }
+  if (selectedValues.length == 3) {
+    final = stat7;
+  }
 
+  db.query(final, (err, rows) => {
+    if (!err) {
+      var result = JSON.parse(JSON.stringify(rows));
+    } else {
+      console.log(err);
+    }
+    res.send(result);
+  });
 });
+
+app.post("/login", (req, res) => {});
 
 const PORT = 4000; // backend routing port
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
-
